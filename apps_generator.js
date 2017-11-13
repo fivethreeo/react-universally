4c810982ba75ce31935c75893b15c259688f1500
Øyvind Saltvik <oyvind.saltvik@gmail.com>
2017-11-13 01:52:28 +0100

    more readable scaffold code

--- apps_generator.js
+++ apps_generator.js
@@ -55,141 +55,155 @@ function write_apps(doc) {
       let actions = [];
       let actions_actions = [];
       let action_creators = [];
-      for (let action of app.actions) {
+
+      for (let action of app.actions || []) {
         let action_name = makeActionName(action.name);
         let action_args = action.args ? action.args.split(/,/g) : [];
 
-        let creator_args_str =
-          action_args.length > 0
-            ? action_args.length > 1
-              ? '(' + action_args.join(', ') + ') => '
-              : action_args[0] + ' => '
-            : '';
+        let creator_args_str = '';
+        if (action_args.length > 0) {
+          if (action_args.length > 1) {
+            let joined_args = action_args.join(', ');
+            creator_args_str = `(${joined_args}) => `;
+          } else {
+            creator_args_str = `${action_args[0]} => `;
+          }
+        }
 
         if (!action.thunk) {
           actions.push(action_name);
-          actions_actions.push('export ' + action_name + ' = ' + "'" + action_name + "';");
+          actions_actions.push(`export ${action_name} = '${action_name}';`);
 
-          let payload =
-            action_args.length > 0
-              ? ',\npayload: ' +
-                (action_args.length > 1
-                  ? '{' + indentString('\n' + action_args.join(', \n'), 2) + '\n}'
-                  : action_args[0])
-              : '';
-
+          let payload = '';
+          if (action_args.length > 0) {
+            payload = ',\npayload: ';
+            if (action_args.length > 1) {
+              let joined_args = action_args.join(', \n');
+              let indented_payload = indentString(`\n${joined_args}`, 2);
+              payload += `{${indented_payload}\n}`;
+            } else {
+              payload += action_args[0];
+            }
+          }
+          let indented_object = indentString(`type: ${action_name}${payload}`, 2);
+          let indented_function = indentString(`return {\n${indented_object}\n};`, 2);
           action_creators.push(
-            'export const ' +
-              action.name +
-              ' => ' +
-              creator_args_str +
-              '{\n' +
-              indentString(
-                'return {\n' + indentString('type: ' + action_name + payload, 2) + '\n};',
-                2,
-              ) +
-              '\n};',
+            `export const ${action_name} => ${creator_args_str} {\n${indented_function}\n};`,
           );
         } else {
-          let thunk_args = action.thunk.args.split(/,/);
-          let thunk_args_str = '{' + thunk_args.join(', ') + '}';
+          let thunk_args = action.thunk.args.split(/,/).join(', ');
+          let thunk_args_str = thunk_args ? `, {${thunk_args}}` : '';
+          let indented_inner_function = indentString(action.thunk.thunk, 2);
+          let indented_outer_function = indentString(
+            `return (dispatch, getState${thunk_args_str}) => {\n${indented_inner_function}};`,
+            2,
+          );
+
           action_creators.push(
-            'export const ' +
-              action.name +
-              ' => ' +
-              creator_args_str +
-              '{\n' +
-              indentString(
-                'return (dispatch, getState, ' +
-                  thunk_args_str +
-                  ') => {\n' +
-                  indentString(action.thunk.thunk, 2) +
-                  '};',
-                2,
-              ) +
-              '\n};',
+            `export const ${action.name} => ${creator_args_str} {\n${indented_outer_function}\n};`,
           );
         }
       }
       let reducers = [];
+      let reducers_names = [];
       let reducers_actions = [];
-      for (let reducer of app.reducers) {
-        let switches = [];
+      for (let reducer of app.reducers || []) {
+        reducers_names.push(reducer.name);
+        let cases = [];
         for (let reducer_action of reducer.actions) {
-          reducers_actions.push(makeActionName(reducer_action.name));
-          switches.push(
-            'case ' +
-              makeActionName(reducer_action.name) +
-              ':\n' +
-              indentString(reducer_action.reducer, 2),
-          );
+          let action_name = makeActionName(reducer_action.name);
+          reducers_actions.push(action_name);
+          let indented_case = indentString(reducer_action.reducer, 2);
+          cases.push(`case ${action_name}:\n${indented_case}`);
         }
-        switches.push('default:\n', indentString('return state', 2));
+        cases.push('default:\n', indentString('return state', 2));
+        let joined_cases = cases.join('');
+        let indented_switch = indentString(`switch (action.type) {\n${joined_cases}\n}`, 2);
         reducers.push(
-          'function ' +
-            reducer.name +
-            '(state = ' +
-            reducer.default +
-            ', action) {\n' +
-            indentString('switch (action.type) {\n' + switches.join('') + '\n}', 2) +
-            '\n};',
+          `function ${reducer.name} (state = ${reducer.default}, action) {\n${indented_switch}\n};`,
         );
       }
-      let models = [];
-      for (let model of app.models) {
-        let model_dir = app_dir + 'models/';
-        mkdirp(model_dir, function(err) {
+
+      let model_dir = app_dir + 'models/';
+
+      mkdirp(model_dir, function(err) {
+        let models = [];
+        for (let model of app.models || []) {
+          models.push(model.name);
+
           let model_reducers_actions = [];
-          let switches = [];
+          let cases = [];
           for (let reducer_action of model.actions) {
-            model_reducers_actions.push(makeActionName(reducer_action.name));
-            switches.push(
-              'case ' +
-                makeActionName(reducer_action.name) +
-                ':\n' +
-                indentString(reducer_action.reducer, 2),
-            );
+            let reducer_action_name = akeActionName(reducer_action.name);
+            model_reducers_actions.push(reducer_action_name);
+            let indented_case = indentString(reducer_action.reducer, 2);
+            cases.push(`case ${reducer_action_name}:\n${indented_case}`);
           }
+
+          let joined_cases = cases.join('');
+          let indented_switch = indentString(`switch (action.type) {\n${joined_cases}\n}`, 2);
           let model_reducer = indentString(
-            'static reducer(state, action, ' +
-              model.name +
-              ', session) {\n' +
-              indentString('switch (action.type) {\n' + switches.join('') + '\n}', 2) +
-              '\n};',
+            `static reducer(state, action, ${model.name}, session) {\n${indented_switch}\n};`,
             2,
           );
+
+          let fields = '';
+          Object.keys(model.fields).forEach(function(key) {
+            fields += key + ': ' + model.fields[key] + ',\n';
+          });
+          fields = indentString(fields, 2);
+
+          let indented_model_actions = indentString(model_reducers_actions.join(',\n'), 2);
+          let model_actions_import = `import {\n${indented_model_actions}\nfrom '../actions';\n\n`;
           write_file(
             model_dir,
             model.name,
-            'import {\n' +
-              indentString(model_reducers_actions.join(',\n'), 2) +
-              "\n} from '../actions';\n\n" +
-              'export class ' +
-              model.name +
-              ' extends Model {\n' +
-              model_reducer +
-              '\n}',
+            `import { Model, many, fk, Schema } from 'redux-orm';` +
+              `export class ${model.name} extends Model {\n${model_reducer}\n}\n\n` +
+              `${model.name}.modelName = '${model.name}';\n\n` +
+              `${model.name}.fields = {\n${fields}};\n\n` +
+              `export default ${model.name};\n`,
           );
-        });
-      }
+        }
+        let model_imports = models
+          .map(function(m) {
+            return `import * as ${m} from './${m}';`;
+          })
+          .join('\n');
+        let models_list = models.join(', ');
+        write_file(
+          model_dir,
+          'index',
+          `${model_imports}\n\n` +
+            `export const schema = new Schema();\n` +
+            `schema.register(${models_list});\n\n` +
+            `export default schema;\n`,
+        );
+      });
 
       write_file(app_dir, 'actions', actions_actions.join('\n\n'));
+
+      let prepend = app.actioncreators_prepend;
+      prepend = prepend ? `${prepend}\n\n` : '';
+      let joined_actions = indentString(actions.join(',\n'), 2);
+      let joined_action_creators = action_creators.join('\n\n');
       write_file(
         app_dir,
         'creators',
-        'import {\n' +
-          indentString(actions.join(',\n'), 2) +
-          "\n} from './actions';\n\n" +
-          action_creators.join('\n\n') +
-          '\n',
+        `import {${joined_actions}\n\n} from './actions';\n\n${prepend}${joined_action_creators}\n`,
       );
+
+      let reducers_prepend = app.reducers_prepend;
+      reducers_prepend = reducers_prepend ? `${reducers_prepend}\n\n` : '';
+      let joined_reducer_actions = indentString(reducers_actions.join(',\n'), 2);
+      let joined_reducers = reducers.join('\n\n');
+      let joined_reducers_names = reducers_names.join(', ');
       write_file(
         app_dir,
         'reducers',
-        'import {\n' +
-          indentString(reducers_actions.join(',\n'), 2) +
-          "\n} from './actions';\n\n" +
-          reducers.join('\n\n'),
+        `import { combineReducers } from \'redux\';\n` +
+          `import {\n${joined_reducer_actions}\n} from './actions';\n\n${reducers_prepend}${joined_reducers}` +
+          `\n\nexport default reducer = combineReducers(${joined_reducers_names});\n`,
       );
     });
   }
